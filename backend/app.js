63b3cef3bc5b7a10bb71726858532479d53c0e81
Apil Adhikari <apiladhikari.social@gmail.com>
2025-04-17 07:11:40 +0545

    Feature: served static files from public folder

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import userRouter from './routes/userRoutes.js';
 import authRouter from './routes/authRoutes.js';
@@ -7,6 +9,13 @@ const app = express();
 
 // To parse req.body use need to use express.json() middleware
 app.use(express.json());
+
+// Serving static files from 'public' directory
+
+const __filename = fileURLToPath(import.meta.url); //only for esmodule
+const __dirname = path.dirname(__filename); //only for esmodule
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Mounting Routers using middleware(syntax: app.use() this is a middleware)
 app.use('/api/v1/auth', authRouter);
